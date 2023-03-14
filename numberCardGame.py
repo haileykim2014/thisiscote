e637a1122c70ee71629baea953e47cb842115119
haileykim2014 <haileykim2014@gmail.com>
2023-03-15 00:46:25 +0900

    그리디- 숫자카드 게임

---
+++ numberCardGame.py
@@ -0,0 +1,12 @@
+# n, m 을 공백으로 구분하여 입력받기
+n, m = map(int,input().split())
+
+result = 0
+for i in range(n):
+    # 현재 줄에서 가장 작은 수 찾기
+    data = list(map(int,input().split()))
+    # 가장 작은 수 들 중에서 가장 큰 수 찾기
+    min_value = min(data)
+    result = max(result,min_value)
+
+print(result)
